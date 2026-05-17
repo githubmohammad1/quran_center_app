027c56dd6daaad089d793e954dd172dcefb8f2b6
Mohammad <mohammadsaeededrees0@gmail.com>
2026-05-17 21:50:06 +0300

    Add GitHub Actions workflow

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,7 +3,7 @@ import 'package:quran_center_app/main.dart';
 
 void main() {
   testWidgets('App loads without crashing', (tester) async {
-    await tester.pumpWidget(MyApp());
-    expect(find.byType(MyApp), findsOneWidget);
+    await tester.pumpWidget(QuranCenterApp());
+    expect(find.byType(QuranCenterApp), findsOneWidget);
   });
 }
